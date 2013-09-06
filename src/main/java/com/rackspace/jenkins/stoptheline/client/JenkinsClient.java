38873725c98ab82ca5907ec956ec08961a03e44e
Kushala Kondapally <kushala.kondapally@rackspace.com>
2013-09-06 12:15:14 -0500

    added jenkins client

---
+++ src/main/java/com/rackspace/jenkins/stoptheline/client/JenkinsClient.java
@@ -0,0 +1,23 @@
+package com.rackspace.jenkins.stoptheline.client;
+
+
+import com.offbytwo.jenkins.model.Job;
+import com.offbytwo.jenkins.model.JobWithDetails;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Interface file to the jenkins-client which a java client for Jenkins API
+ */
+public interface JenkinsClient {
+
+    public Map<String, Job> getAllJobs();
+
+    public JobWithDetails getJobDetails(String jobName)  throws IOException;
+
+    public boolean isJobSuccessful(JobWithDetails job);
+
+    public boolean isAllJobsSuccessful(Map<String, Job> jobs)  throws IOException ;
+
+    }
